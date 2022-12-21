ee64e1c3f7a7d3f96ba6b4d6ef4ec9181cf4c4b0
Sergii Franchuk <Franchuksergei@gmail.com>
2022-12-22 00:44:09 +0200

    Add user model and setup project settings

--- blog/settings.py
+++ blog/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "post",
+    "user",
 ]
 
 MIDDLEWARE = [
@@ -99,6 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "user.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
