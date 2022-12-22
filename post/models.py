9806c64e8c97d81acba8add09034ec634e81eab3
Sergii Franchuk <Franchuksergei@gmail.com>
2022-12-22 02:20:06 +0200

    Add models for post app

--- post/models.py
+++ post/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+import blog.settings
+
+
+class BaseBlogModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+
+    class Meta:
+        abstract = True
+
+
+class Post(BaseBlogModel):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    owner = models.ForeignKey(
+        blog.settings.AUTH_USER_MODEL,
+        related_name="posts",
+        on_delete=models.CASCADE
+    )
+    liked_by = models.ManyToManyField(
+        blog.settings.AUTH_USER_MODEL,
+        through="Like",
+        related_name="liked_posts",
+    )
+
+    class Meta:
+        ordering = ("created_at",)
+
+
+class Like(BaseBlogModel):
+    post = models.ForeignKey(Post, related_name="likes", on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        blog.settings.AUTH_USER_MODEL, related_name="likes", on_delete=models.CASCADE
+    )
