4cdab1ab90662d2c54ec98cd4b9f46f870fce186
Sergii Franchuk <franchuksergei@gmail.com>
2022-12-22 08:44:22 +0200

    Add viewset for Post

--- post/models.py
+++ post/models.py
@@ -6,7 +6,7 @@ import blog.settings
 class BaseBlogModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    is_deleted = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
 
     class Meta:
         abstract = True
--- post/views.py
+++ post/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from post.models import Post
+from post.serializers import (
+    PostSerializer,
+    PostListSerializer,
+    PostDetailSerializer
+)
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.filter(is_active=True)
+    serializer_class = PostSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return PostListSerializer
+
+        if self.action == "retrieve":
+            return PostDetailSerializer
+
+        return PostSerializer
