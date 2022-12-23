9433d0fde170777a1e2cf3618e12e436f56feca6
Sergii Franchuk <franchuksergei@gmail.com>
2022-12-23 12:21:06 +0200

    Implemented JWT Token authentication

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 from user.views import CreateUserView
 
 
 urlpatterns = [
     path("signup/", CreateUserView.as_view(), name="signup"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
 ]
 
 app_name = "user"
