56a9ae4ba63a16083edc3a971c655bc7d63de06e
Sergii Franchuk <franchuksergei@gmail.com>
2022-12-23 19:27:59 +0200

    Add me page for user so he can update his info

--- user/urls.py
+++ user/urls.py
@@ -1,14 +1,18 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
 
-from user.views import CreateUserView
-
+from user.views import CreateUserView, ManageUserView
 
 urlpatterns = [
     path("signup/", CreateUserView.as_view(), name="signup"),
-    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("me/", ManageUserView.as_view(), name="manage"),
 ]
 
 app_name = "user"
